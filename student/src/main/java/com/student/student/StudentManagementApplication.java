241ca4db4ec9e27a509dcc1134265f727c44a768
Pratik-Karodade <karodadepratik@gmail.com>
2024-04-06 18:28:58 +0530

    commit 1

--- student/src/main/java/com/student/student/StudentManagementApplication.java
+++ student/src/main/java/com/student/student/StudentManagementApplication.java
@@ -9,4 +9,5 @@ public class StudentManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
+//Hello this is sample for commit
 }
